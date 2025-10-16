c8b5d38fc05f2d98cfb96104f5d33cae49f8dd23
Negede <negedetekleyes@gmail.com>
2025-10-16 17:44:02 +0300

    Update Layout page of admin

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "../../components/Menu"
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -9,6 +10,7 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
+      
         {/*left side  */}
         <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
             <Link href= "/"   className="flex items-center justify-center lg:justify-start gap-2">
@@ -16,9 +18,13 @@ export default function DashboardLayout({
             <span className="hidden lg:block font-bold">ShegaReport</span>
             </Link>
             <Menu/>
+            
         </div>
         {/* right side */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]  bg-[#F7F8FA] overflow-scroll flex flex-col"></div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]  bg-[#F7F8FA] overflow-scroll flex flex-col">
+          <Navbar/>
+          {children}
+        </div>
     </div>
   );
 }
