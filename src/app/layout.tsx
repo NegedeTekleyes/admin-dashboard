6b0c5083444d39f59e75bf5320a3097113807cee
Negede <negedetekleyes@gmail.com>
2025-10-16 17:48:13 +0300

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "./providers/ThemeProvider";
+import { AuthProvider } from "./contexts/AuthContex";
+import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Admin Dashboard",
+  description: "ShegaReport Admin Dashboard",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100`}
+      >
+        <AuthProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
