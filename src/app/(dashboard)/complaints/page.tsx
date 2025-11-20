9190873173f3f08d14e96bca229c01e2df0e75af
Negede <negedetekleyes@gmail.com>
2025-11-20 15:22:51 +0300

    update pagination to move next page

--- src/app/(dashboard)/complaints/page.tsx
+++ src/app/(dashboard)/complaints/page.tsx
@@ -25,6 +25,7 @@ interface Technician {
   };
   speciality: string | null;
   status: string;
+  createdAt: string
 }
 
 export default function AdminComplaintsPage() {
@@ -165,8 +166,11 @@ export default function AdminComplaintsPage() {
   };
 
   const handleFilterChange = (key: string, value: string) => {
-    setFilters(prev => ({ ...prev, [key]: value, page: 1 }));
+    setFilters(prev => ({ 
+      ...prev, 
+      [key]: key === 'page'? parseInt(value) : value}));
   };
+  
 
   const getStatusColor = (status: string) => {
     const colors = {
